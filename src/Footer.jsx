4d1a9431ef5c8a3ca1f43c46c59c8961852e0e66
saimaar <rahman.saima92@gmail.com>
2020-04-10 15:48:14 -0400

    added links to my git

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Image, Header } from 'semantic-ui-react'
+import {Icon, Segment, Button} from 'semantic-ui-react'
 import logo from './images/logo.png'
 
 class Footer extends Component {
@@ -16,6 +17,11 @@ class Footer extends Component {
                 <div className="footer-logo-container">
                     <Image className="footer-image" src={logo} alt="trip advisor logo"/>
                     <Header className="footer-title">TravelAdvisor</Header>
+                </div>
+                <div className="ygl-btn">
+                <Button href="" circular color='youtube' icon='youtube' />
+                 <Button href="https://www.linkedin.com/in/saimi1992/" circular color='linkedin' icon='linkedin' />
+                 <Button href="https://github.com/saimaar/TravelAdvisor" circular color="black" icon='github' />
                 </div>
                 <span className="footer-certified-logo">&copy; {this.actualYear()} TravelAdvisor All rights reserved.</span>
                 <p className="footer-website-rights">TravelAdvisor is not a booking agent or tour operator, and does not charge any service fees to users of our site. Our mission is to provide you the best informations for your traveling. For booking please contact any airlines, travel providers, and booking agents who list airfare, tours, and travel packages. TravelAdvisor</p>
